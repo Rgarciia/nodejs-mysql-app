dca33420581c4942e65c6832cfe21599c6f3d461
Brandon Rene <brandon.rene@noviscorp.com>
2022-02-03 21:18:03 -0600

    Crud Complete

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const mysqlStore = require('express-mysql-session');
+const { database } = require('./keys');
 
 
 //Initialitations
@@ -20,13 +24,23 @@ app.engine('.hbs', engine({
   app.set('view engine', '.hbs');
 
 //Milddwares
+app.use(session({
+  secret:'session',
+  resave: false,
+  saveUninitialized: false,
+  store: new mysqlStore(database)
+
+}))
+app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 
+
 //Global Variables
 app.use((req, res, next) => {
+    app.locals.success = req.flash('success');
     next();
  })
 
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { redirect, links } = require('express/lib/response');
 const res = require('express/lib/response');
 const router = express.Router();
 
@@ -6,7 +7,52 @@ const pool = require('../database');
 router.get('/add', (req, res) => {
     res.render('links/add');
 });
-router.post('/add', (req, res) => {
-    res.send('received');
+router.post('/add', async(req, res) => {
+    const {title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await  pool.query('INSERT INTO links set ?', [newLink]);
+    req.flash('success', 'Link saved successfully');
+    res.redirect('/links');
 });
+
+router.get('/', async (req, res) => {
+    const links = await pool.query('SELECT * FROM links');
+    console.log(links);
+    res.render('links/list.hbs', {links});
+
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE id = ?', [id]);
+    req.flash('success', 'Link removed successfully');
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    res.render('links/edit', {link: links [0]});
+});
+
+router.post('/edit/:id', async(req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = {
+        title,
+        description,
+        url
+
+    };
+    await pool.query('UPDATE links SET ? WHERE id = ?', [newLink, id]);
+    req.flash('success', 'Link updated successfully');
+    res.redirect('/links');
+});
+
+
+
 module.exports = router;
